998c3eea9d23c6a81399a786d865191b695057a5
Tom Collins <tom@tomcollins.co.uk>
2020-08-21 17:23:19 +0100

    feat(templatess): Allow use of templates to be skipped so we output pure resolved json.

--- tests/json-schema-static-docs.test.js
+++ tests/json-schema-static-docs.test.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const rimraf = require("rimraf");
-const _ = require('lodash')
+const _ = require("lodash");
 
-const JsonSchameStaticDocs = require('../lib/json-schema-static-docs.js');
+const JsonSchameStaticDocs = require("../lib/json-schema-static-docs.js");
 
 let defaultTestOptions = {
-  inputPath: './tests/examples/schema/',
-  outputPath: './tests/docs/'
+  inputPath: "./tests/examples/schema/",
+  outputPath: "./tests/docs/",
 };
 let testOptions = {};
 
 beforeEach(() => {
   testOptions = _.cloneDeep(defaultTestOptions);
-  if (!fs.existsSync(testOptions.outputPath)){
+  if (!fs.existsSync(testOptions.outputPath)) {
     fs.mkdirSync(testOptions.outputPath);
   }
 });
@@ -22,27 +22,40 @@ afterEach(() => {
   rimraf.sync(testOptions.outputPath);
 });
 
-test('resolves single schema', async () => {
+test("resolves single schema", async () => {
   const jsonSchameStaticDocs = new JsonSchameStaticDocs(testOptions);
   await jsonSchameStaticDocs.generate();
 });
 
-test('loads single additional data source', async () => {
-  testOptions.additionalDataSources = { 
-    foo: './tests/examples/foo',
-    bar: './tests/examples/bar',
+test("loads single additional data source", async () => {
+  testOptions.additionalDataSources = {
+    foo: "./tests/examples/foo",
+    bar: "./tests/examples/bar",
   };
   const jsonSchameStaticDocs = new JsonSchameStaticDocs(testOptions);
   let mergedSchemas = await jsonSchameStaticDocs.generate();
-  expect(mergedSchemas[0].filename).toBe('tests/examples/schema/name.json');
-  expect(mergedSchemas[0].foo.filename).toBe('tests/examples/foo/name.json');
-  expect(mergedSchemas[0].bar.filename).toBe('tests/examples/bar/name.json');
+  console.log(mergedSchemas);
+  expect(mergedSchemas[0].filename).toBe("tests/examples/schema/name.json");
+  expect(mergedSchemas[0].foo.filename).toBe("tests/examples/foo/name.json");
+  expect(mergedSchemas[0].bar.filename).toBe("tests/examples/bar/name.json");
 });
 
-test('supports custom templates', async () => {
-  testOptions.templatePath = path.join(__dirname, 'examples/templates/');
+test("supports custom templates", async () => {
+  testOptions.templatePath = path.join(__dirname, "examples/templates/");
   const jsonSchameStaticDocs = new JsonSchameStaticDocs(testOptions);
   await jsonSchameStaticDocs.generate();
-  let result = fs.readFileSync(path.join(testOptions.outputPath, 'name.md'));
-  expect(result.toString()).toBe('foo');
+  let result = fs.readFileSync(path.join(testOptions.outputPath, "name.md"));
+  expect(result.toString()).toBe("foo");
+});
+
+test("allows templates to be skipped", async () => {
+  testOptions.skipTemplates = true;
+  const jsonSchameStaticDocs = new JsonSchameStaticDocs(testOptions);
+  await jsonSchameStaticDocs.generate();
+  let result = fs.readFileSync(
+    path.join(testOptions.outputPath, "person.json")
+  );
+  let schema = JSON.parse(result.toString());
+  expect(schema.title).toBe("Person");
+  expect(schema.properties.name.properties.firstNames.type).toBe("string");
 });
